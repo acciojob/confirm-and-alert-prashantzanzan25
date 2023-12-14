0b73b37378a8bb082fcbe4f1158bfccc57f9aa94
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-12-14 11:40:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+var proceed = confirm("Do you want to proceed?");
+
+if (proceed) {
+	alert("You clicked OK. Proceeding...");
+} 
+else {
+	alert("You clicked Cancel. Exiting...");
+}
